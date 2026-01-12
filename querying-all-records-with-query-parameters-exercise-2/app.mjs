b65ebf27cfce8c4247a57be168b0327683d3386d
pangiiz-qq <phebe.phiphi@gmail.com>
2026-01-12 13:04:43 +0700

    feat:search movie by genere and keyword

--- querying-all-records-with-query-parameters-exercise-2/app.mjs
+++ querying-all-records-with-query-parameters-exercise-2/app.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { pool } from "./db.mjs";
+import {pool} from "./db.mjs";
 
 const app = express();
 const port = 4000;
@@ -7,29 +7,29 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
-	try {
-		const genresParam = req.query.genres ? `%${req.query.genres}%` : null;
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const keywordsParam = null;
-		const result = await pool.query(
-			`
+  try {
+    const genresParam = req.query.genres ? `%${req.query.genres}%` : null;
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
+    const keywordsParam = req.query.keyword ? `%${req.query.keywords}%` : null;
+    const result = await pool.query(
+      `
       SELECT * FROM movies 
-      WHERE (genres ILIKE $1 OR $1 IS NULL)  
+      WHERE (genres ILIKE $1 OR $1 IS NULL) AND (title ILIKE $2 or $2 is null) 
       `,
-			[genresParam]
-		);
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
+      [genresParam, keywordsParam]
+    );
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
-		return res.json({
-			data: result.rows,
-		});
-	} catch (e) {
-		return res.json({
-			message: "ไม่สามารถเชื่อมต่อ Database ได้",
-		});
-	}
+    return res.json({
+      data: result.rows,
+    });
+  } catch (e) {
+    return res.json({
+      message: "ไม่สามารถเชื่อมต่อ Database ได้",
+    });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`🚀 Server is running at ${port}`);
+  console.log(`🚀 Server is running at ${port}`);
 });
