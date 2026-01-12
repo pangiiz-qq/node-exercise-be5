1e18c83a13146f890c924e580f6111f9671df164
pangiiz-qq <phebe.phiphi@gmail.com>
2026-01-12 12:28:09 +0700

    feat: movies filter by genres

--- querying-all-records-with-query-parameters-exercise-1/app.mjs
+++ querying-all-records-with-query-parameters-exercise-1/app.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { pool } from "./db.mjs";
+import {pool} from "./db.mjs";
 
 const app = express();
 const port = 4000;
@@ -7,27 +7,28 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
-	try {
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const genresParam = null;
-		const result = await pool.query(
-			`
+  try {
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
+    const genresParam = req.query.genres ? `%${req.query.genres}%` : null;
+    const result = await pool.query(
+      `
     SELECT * FROM movies
+	WHERE (genres LIKE $1 OR $1 IS NULL)
     `,
-			[]
-		);
-		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
+      [genresParam]
+    );
+    // แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
-		return res.json({
-			data: result.rows,
-		});
-	} catch (e) {
-		return res.json({
-			message: "ไม่สามารถเชื่อมต่อ Database ได้",
-		});
-	}
+    return res.status(200).json({
+      data: result.rows,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "ไม่สามารถเชื่อมต่อ Database ได้",
+    });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`🚀 Server is running at ${port}`);
+  console.log(`🚀 Server is running at ${port}`);
 });
